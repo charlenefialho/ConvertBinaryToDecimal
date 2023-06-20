561d1b62304c3879bcaa501bb088638b5088a7a4
charlenefialho <fialhocharlene@gmail.com>
2023-06-20 07:59:25 -0300

    feat: created binary to decimal conversion function

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-function sum(a, b){
-    return a + b;
-}
+const converterButton = document.querySelector("#converterButton");
+const outputDecimalNumber = document.querySelector("#outputDecimalNumber");
 
-console.log(sum(2,2));
+converterButton.addEventListener('click', callFunctionConvertBinaryToDecimal);
+
+function callFunctionConvertBinaryToDecimal(){
+    const inputBinaryNumber = document.querySelector("#inputBinaryNumber");
+    const inputValue = inputBinaryNumber.value;
+    convertBinaryToDecimal(inputValue);
+ }
+
+function convertBinaryToDecimal(inputValue){
+    const newArray = [];
+    for(let i = 0; i< inputValue.length; i++){
+        if(inputValue[i] === '0'){
+            newArray[i] = 0;
+        }else if(inputValue[i] === '1'){
+            newArray[i] = 1;
+        }else{
+            console.log("Não é número binário");
+        }
+    }
+        let total = 0, i = 0;
+       for(let n = newArray.length; n > 0; n--){
+         total += newArray[i] * Math.pow(2,n - 1);
+         i += 1;
+       }
+       outputDecimalNumber.innerHTML = `Numero Decimal: ${total}`;
+     }
