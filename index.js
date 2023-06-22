4db45b886d1930d5ac1ecaa838eade6e185e1714
charlenefialho <fialhocharlene@gmail.com>
2023-06-22 07:38:29 -0300

    feat:update verification binary number

--- index.js
+++ index.js
@@ -12,27 +12,37 @@ function callFunctionConvertBinaryToDecimal(){
 
 function convertBinaryToDecimal(inputValue){
     const binaryNumberArray = [];
+    let isBinaryNumber = false;
     for(let i = 0; i< inputValue.length; i++){
         if(inputValue[i] === '0'){
             binaryNumberArray[i] = 0;
+            isBinaryNumber = true;
             notBinaryNumber.innerHTML = ' ';
             notBinaryNumber.style.color = 'red'
 
         }else if(inputValue[i] === '1'){
             binaryNumberArray[i] = 1;
+            isBinaryNumber = true;
             notBinaryNumber.innerHTML = ' ';
             notBinaryNumber.style.color = 'red'
 
         }else{
+            isBinaryNumber = false;
             notBinaryNumber.innerHTML = 'Insira um número binário válido';
             notBinaryNumber.style.color = 'red'
+            break;
            
         }
     }
         let total = 0, i = 0;
-       for(let n = binaryNumberArray.length; n > 0; n--){
-         total += binaryNumberArray[i] * Math.pow(2,n - 1);
-         i += 1;
-       }
-       outputDecimalNumber.innerHTML = `Número Decimal: ${total}`;
+        if(isBinaryNumber){
+            for(let n = binaryNumberArray.length; n > 0; n--){
+                total += binaryNumberArray[i] * Math.pow(2,n - 1);
+                i += 1;
+              }
+              outputDecimalNumber.innerHTML = `Número Decimal: ${total}`;
+        }else{
+            outputDecimalNumber.innerHTML = 'Número Decimal: ';
+        }
+       
      }
