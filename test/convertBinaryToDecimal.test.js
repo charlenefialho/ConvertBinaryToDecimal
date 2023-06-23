86bf0c747729169ef65c8a5295bd36ec49217c5e
charlenefialho <fialhocharlene@gmail.com>
2023-06-23 14:41:25 -0300

    fix: changes ../convertBinaryToDecimal to ../test/convertBinaryToDecimal

--- test/convertBinaryToDecimal.test.js
+++ test/convertBinaryToDecimal.test.js
@@ -1,4 +1,4 @@
-const convertBinaryToDecimal = require('../convertBinaryToDecimal');
+const convertBinaryToDecimal = require('../test/convertBinaryToDecimal');
 
 test('adds [1,0] to equal 2', () => {
   expect(convertBinaryToDecimal([1,0])).toBe(2);
